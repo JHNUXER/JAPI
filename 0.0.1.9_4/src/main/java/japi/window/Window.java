d7288067e7e855c086ba3a738eca5e8e297e5d9a
John Kayne <johnhoganuke2@gmail.com>
2016-11-15 10:36:41 -0500

    Update Window.java

--- 0.0.1.9_4/src/main/java/japi/window/Window.java
+++ 0.0.1.9_4/src/main/java/japi/window/Window.java
@@ -1,2 +1,16 @@
 package japi.window;
 
+public abstract class Window extends JFrame {
+  public boolean fullscreen;
+  public void setFullscreen(boolean fs) {
+    this.fullscreen = fs;
+    setUndecorated(fs);
+    if (fs) setExtendedState(JFrame.MAXIMIZED_BOTH);
+    if (!fs) setExtendedState(new JFrame.getExtendedState());
+  }
+  public void toggleFullscreen() {
+    setFullscreen(!fullscreen);
+  }
+  
+  public void close();
+}
